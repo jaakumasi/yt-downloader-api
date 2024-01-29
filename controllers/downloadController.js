b6a81d9acd75d95c48e6f9d182c6a973900ed086
jaakumasi <jerome.akumasi@amalitech.com>
2024-01-29 14:09:27 +0000

    download controller modified

--- controllers/downloadController.js
+++ controllers/downloadController.js
@@ -1,21 +1,39 @@
-const { createReadStream, createWriteStream } = require('fs');
+const { createReadStream, createWriteStream, unlink } = require('fs');
 const { join } = require('path')
 const ytdl = require('ytdl-core');
+const { logger } = require('../shared/utils')
 
 const handleDownloadController = (req, res, next) => {
     const { ytVideoUrl } = req.body;
 
-    const randomID = Math.round(Math.random() * 1000000).toString().padStart(6);
+    console.log(ytVideoUrl)
+
+
+    const randomID = Math.round(Math.random() * 1000000).toString().padStart(6, '0');
     const videoPath = join(__dirname, '..', `videos/video-${randomID}.mp4`);
 
-    const writeStream = createWriteStream(videoPath);
-    ytdl(ytVideoUrl).pipe(writeStream);
+    try {
+        const writeStream = createWriteStream(videoPath);
+        ytdl(ytVideoUrl).pipe(writeStream);
 
-    writeStream.on('finish', () => {
-        res.setHeader('Content-Type', 'video/mp4');
-        const stream = createReadStream(videoPath);
-        stream.pipe(res);
-    })
+        writeStream.on('finish', () => {
+            logger('write complete! initiating download');
+
+            res.setHeader('Content-Type', 'video/mp4');
+            const readStream = createReadStream(videoPath);
+            readStream.pipe(res);
+
+            readStream.on('finish', () => {
+                unlink(videoPath, (err) => {
+                    if (err) logger(err.message);
+                });
+            })
+        });
+        writeStream.on('ready', () => logger('ready'))
+        writeStream.on('error', (error) => logger(error.message));
+    } catch (error) {
+        logger(error.message)
+    }
 }
 
 module.exports = handleDownloadController; 
