1ef24956010129642ff50bc1ce4d150437dacf3a
jaakumasi <jerome.akumasi@amalitech.com>
2024-01-29 14:09:55 +0000

    verify video url

--- controllers/verifyUrlController.js
+++ controllers/verifyUrlController.js
@@ -1,10 +1,12 @@
 const ytdl = require("ytdl-core");
 
 const handleVerifyUrlController = (req, res, next) => {
-
     const { ytVideoUrl } = req.body;
-    console.log(ytVideoUrl)
+    console.log('in verify controller')
+
     const isValid = ytdl.validateURL(ytVideoUrl);
+    console.log(isValid)
+
 
     res.json({ isValid });
 }
